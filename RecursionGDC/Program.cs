0edb94fb28e51896ee70f04d722c985378465cc3
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-06 13:30:35 +0200

    Recursion GDC

---
+++ RecursionGDC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionGDC
+{
+    class Program
+    {
+        static int GreatestCommonDivisor(int value1, int value2)
+        {
+
+            if (value2 == 0) return value1;
+
+            return GreatestCommonDivisor(value2, value1 % value2);
+        }
+
+
+        static void Main(string[] args)
+        {
+            int devisor = GreatestCommonDivisor(345, 150);
+
+            Console.WriteLine("Devisor: " + devisor);
+        }
+
+    }
+}
