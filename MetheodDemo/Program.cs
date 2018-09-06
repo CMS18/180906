c67b36348fb830d7b2b84091cd2ce4d72abb0437
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-06 11:38:18 +0200

    Methods

--- MetheodDemo/Program.cs
+++ MetheodDemo/Program.cs
@@ -8,8 +8,71 @@ namespace MetheodDemo
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static double GetRectangleArea(ref double width, ref double height)
         {
+            double area = width * height;
+            width = width * 2;
+            return area;
+        }
+
+
+        static double GetRectangleArea(int width, int height)
+        {
+            double area = width * height;
+            return area;
+        }
+
+        static int SumArray(int[] data2)
+        {
+            data2 = new int[10];
+            data2[0] = 123455;
+
+            return 0;
+        }
+    
+        static void MockString(ref string aString)
+        {
+            // aString[0] = 'f'; --- Does not work. Strings are immutable.
+            Console.WriteLine(aString);
+            aString = "Test";
+            Console.WriteLine(aString);
+
+        }
+
+
+        static void Main(string[] args)
+        {   
+            //Reference Type Demo
+            int[] data = new[] { 1, 2, 3 };
+            SumArray(data);
+
+            // String (Reference Type Demo
+            string str1 = "Fredrik";
+            MockString(ref str1);
+
+            double width;
+            double height;
+            bool ok;
+
+            GetDoubleValue("Width? ", out width);
+            GetDoubleValue("Height? ", out height);
+
+            Console.WriteLine(GetRectangleArea(ref width, ref height));
+            Console.WriteLine(GetRectangleArea(ref width, ref height));
+
+        }
+
+        private static void GetDoubleValue(string prompt, out double value)
+        {
+            bool ok;
+            Console.Write(prompt);
+            do
+            {
+                string text = Console.ReadLine();
+                ok = double.TryParse(text, out value);
+
+            } while (!ok);
         }
     }
 }
---
+++ MinMaxProblem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMaxProblem
+{
+    class Program
+    {
+        static void HittaLägstaOchHögstaTal(int tal1, int tal2, int tal3, out int lägstaTalet, out int högstaTalet)
+        {
+
+            högstaTalet = 0;
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+
+            HittaLägstaOchHögstaTal(5, 3, 7, högstaTalet: out max, lägstaTalet: out min);
+
+            Console.WriteLine("Lägst: "+ min);
+
+            Console.WriteLine("Högst: " + max);
+
+        }
+    }
+}
