1855ed1484421f55e95a191f08f8a4ad3743e81d
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-06 12:02:49 +0200

    MinMax

--- MinMaxProblem/Program.cs
+++ MinMaxProblem/Program.cs
@@ -8,11 +8,53 @@ namespace MinMaxProblem
 {
     class Program
     {
-        static void HittaLägstaOchHögstaTal(int tal1, int tal2, int tal3, out int lägstaTalet, out int högstaTalet)
+        static void HittaLägstaOchHögstaTal(int tal1, int tal2, int tal3, 
+                                            out int lägstaTalet, out int högstaTalet)
         {
+            if (tal1 >= tal2 && tal1 >= tal3)
+            {
+                högstaTalet = tal1;
+            }
+            else if (tal2 >= tal1 && tal2 >= tal3)
+            {
+                högstaTalet = tal2;
+            }
+            else 
+            {
+                högstaTalet = tal3;
+            }
 
-            högstaTalet = 0;
-            return 0;
+            if (tal1 <= tal2 && tal1 <= tal3)
+            {
+                lägstaTalet = tal1;
+            }
+            else if (tal2 <= tal1 && tal2 <= tal3)
+            {
+                lägstaTalet = tal2;
+            }
+            else
+            {
+                lägstaTalet = tal3;
+            }
+        }
+
+        static void HittaLägstaOchHögstaTal2(int tal1, int tal2, int tal3,
+                                             out int lägstaTalet, out int högstaTalet)
+        {
+            högstaTalet = (tal1 >= tal2) ? 
+                            (tal1 >= tal3 ? tal1 : tal3) : 
+                            (tal2 >= tal3 ? tal2 : tal3);
+
+            lägstaTalet = (tal1 <= tal2) ? tal1 : tal2;
+            lägstaTalet = (lägstaTalet <= tal3) ? lägstaTalet : tal3;
+        }
+
+        static void HittaLägstaOchHögstaTal3(int tal1, int tal2, int tal3,
+                                             out int lägstaTalet, out int högstaTalet)
+        {
+            högstaTalet = Math.Max(tal1, Math.Max(tal2, tal3));
+
+            lägstaTalet = Math.Min(tal1, Math.Min(tal2, tal3));
         }
 
 
@@ -21,7 +63,7 @@ namespace MinMaxProblem
             int min;
             int max;
 
-            HittaLägstaOchHögstaTal(5, 3, 7, högstaTalet: out max, lägstaTalet: out min);
+            HittaLägstaOchHögstaTal2(1, 3, 7, högstaTalet: out max, lägstaTalet: out min);
 
             Console.WriteLine("Lägst: "+ min);
 
