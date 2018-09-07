24ffcb001583b8272113deb7adc322348fa0efa3
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-07 11:11:42 +0200

    Objekt demo

---
+++ ObjectDemo/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDemo
+{
+    class Person
+    {
+        // Fields => variabler
+
+        private string name;
+        private int _age;
+
+        // Properties
+
+        public string Name
+        {
+            // public string getName()
+            get
+            {
+                return name;
+            }
+
+            // public void setName(string value)
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Namn får inte vara tomt");
+                }
+                name = value;
+            }
+        }
+
+        public int Age { get => _age; set => _age = value; }
+
+        public int ShowSize { get; set; }
+
+        // Constructor
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        // Methods
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("Person");
+            Console.WriteLine("Name: " + this.Name);
+        }
+    }
+}
---
+++ ObjectDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Fredrik");
+            
+            p1.PrintPerson();
+
+            Person p2 = new Person("Sandra");
+            p2.Name = "Sandra Kristina";
+            p2.PrintPerson();
+
+            p1 = null;
+        }
+    }
+}
